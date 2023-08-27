0cd691335276dbc32d08bb32eff92cdb3d54e82f
Anthony Hein <anthonyhein@Anthonys-MacBook-Pro.local>
2023-08-27 11:28:05 -0400

    remove cruft

--- resources/js/show-projects-script.js
+++
@@ -1,9 +0,0 @@
-function showHideProj(projNum) {
-    var id = "proj" + projNum;
-    var x = document.getElementById(id);
-    if (x.style.display === "none" || x.style.display === "") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
-}
